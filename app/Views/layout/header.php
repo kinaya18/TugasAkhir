82a0e4ec4b4ceb397d324e6e4f0bb205c26164f8
DaraRahma536 <dararahma536@gmail.com>
2026-06-10 22:26:39 +0700

    nambah user-scalable=yes

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 
 <title><?= $title ?? 'Pemantau Kualitas Udara' ?></title>
 
